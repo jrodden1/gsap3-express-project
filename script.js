71152122ddb5f69f6dcb05b9c6e9343462552210
Jeremiah Rodden <jrodden1@gmail.com>
2020-01-20 19:29:55 -0700

    Added in basic animations for each element

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 //https://www.freepik.com/free-photos-vectors/business">Business vector created by freepik - www.freepik.com
 // my very basic animation from memory (it works!)
-gsap.timeline()
-   .from("#items g", {opacity: 0, duration: 1, stagger: 0.1})
+let tl = gsap.timeline()
+   tl.from("#demo", {opacity: 0, duration: 1})
+     .from("h1", {x:80, opacity: 0})
+     .from("h2", {x:-80, opacity: 0})
+     .from("p", {y:30, opacity: 0})
+     .from("button", {y:30, opacity:0})
+     .from("#items > g", {scale: 0, opacity: 0, transformOrigin: "50% 50%", stagger: 0.1})
