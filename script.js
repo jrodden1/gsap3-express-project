6655bf1438ddb57665ad6a7b33ee8472d46f7e59
Jeremiah Rodden <jrodden1@gmail.com>
2020-01-20 19:38:07 -0700

    utilized the defaults object on the timeline for opacity; removed opacity from tweens due to this

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 //https://www.freepik.com/free-photos-vectors/business">Business vector created by freepik - www.freepik.com
 // my very basic animation from memory (it works!)
-let tl = gsap.timeline()
-   tl.from("#demo", {opacity: 0, duration: 1})
-     .from("h1", {x:80, opacity: 0})
-     .from("h2", {x:-80, opacity: 0})
-     .from("p", {y:30, opacity: 0})
-     .from("button", {y:30, opacity:0})
-     .from("#items > g", {scale: 0, opacity: 0, transformOrigin: "50% 50%", stagger: 0.1})
+let tl = gsap.timeline({
+   // you can add default values for every tween inside of this timeline 
+   defaults: {
+      opacity: 0
+   }
+})
+
+   // once you add those defaults you can then exclude that information from each tween unless you want to manually override it.
+   tl.from("#demo", {duration: 1})
+     .from("h1", {x:80})
+     .from("h2", {x:-80}, "<") // Told this to come in at the same time as the previous tween
+     .from("p", {y:30})
+     .from("button", {y:30})
+     .from("#items > g", {scale: 0, transformOrigin: "50% 50%", stagger: 0.1})
      // transformOrigin allows you to scale from the center anchor point vs the default of x:0, y:0
