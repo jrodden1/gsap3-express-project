8c7f20e50a33b8d54292bb7e4459b2261fd0b6b2
Jeremiah Rodden <jrodden1@gmail.com>
2020-01-20 19:23:34 -0700

    test timeline for animating in groups within the items group

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
 //https://www.freepik.com/free-photos-vectors/business">Business vector created by freepik - www.freepik.com
+// my very basic animation from memory (it works!)
+gsap.timeline()
+   .from("#items g", {opacity: 0, duration: 1, stagger: 0.1})
